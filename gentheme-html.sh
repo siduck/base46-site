d6603a9664521234aae4fc17b37017571bc016b8
siduck <siduck@tutanota.com>
2025-01-03 09:41:33 +0530

    gitignore vihtml

---
+++ gentheme-html.sh
@@ -0,0 +1,15 @@
+echo "Setting up nvim"
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar -zxf nvim-linux64.tar.gz
+mv nvim-linux64 nv
+
+mkdir ~/.config
+cp -r tiny ~/.config/nvim
+
+mkdir src/lib/vihtml
+
+echo "Install treesitter parsers"
+nv/bin/nvim --headless +"q"
+
+echo "Generating html from themes"
+nv/bin/nvim --headless +":lua require 'vihtml'" +"q"
